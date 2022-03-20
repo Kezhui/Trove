dbd8fefc63ac9cad8d3d98be8314dfa856e08595
Kezhui <100781350+Kezhui@users.noreply.github.com>
2022-03-20 20:40:26 -0300

    Update autoAccept.py

--- autoAccept/autoAccept.py
+++ autoAccept/autoAccept.py
@@ -6,12 +6,12 @@ aceitar = 'aceitar.png'
 
 def autoAccept(): 
     cont = 1                
-    while True:                                                     #Loop inifinito    
-        accept = pyautogui.locateOnScreen(aceitar,confidence=0.9)   #Localiza a imagem na tela
+    while True:                                                     #Loop infinito    
+        accept = pyautogui.locateOnScreen(aceitar,confidence=0.9)   #Localiza a imagem na tela, com uma precisa de  0.9 (90%)
         if accept:                                                  #Se conseguir localizar
             pyautogui.moveTo(accept)                                #Move o mouse até o meio da onde foi localizado
             pydirectinput.click()                                   #Clica no mesmo lugar onde ta o mouse
-            print(f"CONVITE ACEITO ({cont})")       
+            print(f"CONVITE ACEITO ({cont})")                       #Printa a quantidade de vezes que foi aceito até agora
             cont += 1                                               #Aumenta 1 no contador
             time.sleep(1)                                           #Espera 1 segundo e repete o loop
 
